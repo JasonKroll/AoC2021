ea3156454bfaedce577d554ec80384c061cde61b
Jason Kroll <krolly@gmail.com>
2021-12-04 15:02:00 +1000

    Some mac stuff

--- AoC2021.Core/Loader.cs
+++ AoC2021.Core/Loader.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AoC2021.Core
+﻿namespace AoC2021.Core
 {
-    /// <summary>
-    /// Load test data
-    /// </summary>
-    /// <remarks>
-    /// Expects test data to be in the format {typename}Test.txt e.g. Day01Test.txt
-    /// Expects real data to be in the format {typename}.txt e.g. Day01Test.txt
-    /// </remarks>
-    public static class Loader
+  /// <summary>
+  /// Load test data
+  /// </summary>
+  /// <remarks>
+  /// Expects test data to be in the format {typename}Test.txt e.g. Day01Test.txt
+  /// Expects real data to be in the format {typename}.txt e.g. Day01Test.txt
+  /// </remarks>
+  public static class Loader
     {
         public static List<string> LoadTestData(Type type)
         {
